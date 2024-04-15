6d79f30bac4b5acc7dc30159d62755d3b202291d
Selena X <selenaxx20@gmail.com>
2024-04-14 23:59:56 -0500

    completed

--- longestSubstringWithoutRepeatingChararcters.js
+++ longestSubstringWithoutRepeatingChararcters.js
@@ -3,17 +3,16 @@
 Given a string s, find the length of the longest substring without repeating characters.
 
 Example 1:
-
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
-Example 2:
 
+Example 2:
 Input: s = "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
-Example 3:
 
+Example 3:
 Input: s = "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3.
@@ -25,31 +24,23 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 // O(N)
 // find longest block of characters without any repeats, return length
 const lengthOfLongestSubstring = (str) => {
-  // create str
-  // create variable of max to keep track of str's length
-
-  let subStr = '';
+  let hash = {};
   let max = 0;
-
+  let start = 0;
+  //assign current char as key, index as value
+  //if duplicate char, start again from last index of char + 1
   for (let i = 0; i < str.length; i++) {
-    // accumulate of unique char on each iteration
-    if (subStr.indexOf(str[i]) === -1) {
-      subStr += str[i];
-      // compare max to str length, if length is bigger, assign length to be next max
-      max = Math.max(max, subStr.length);
-    } else {
-      // if char already exist, restart str at the previous index of curr char
-      // curr index minus substr length and add 1 to account for index
-      // account for current substr's length
-      //   console.log(str.substring(1));
-      subStr = subStr.substring(i - subStr.length + 1); //1 - 1 + 1
-      console.log(subStr, i, '💕');
-      max = Math.max(max, subStr.length);
+    let currChar = str[i];
+    if (hash[currChar]) {
+      start = hash[currChar] + 1;
     }
+    hash[currChar] = i;
+    max = Math.max(max, i - start + 1);
   }
-  // return max after loop finishes.
   return max;
 };
 
-// console.log(lengthOfLongestSubstring('ddwfded')); //wfde 4
-console.log(lengthOfLongestSubstring('aab')); //ab 2
+console.log(lengthOfLongestSubstring('ddwfed')); //4
+console.log(lengthOfLongestSubstring('abccd')); //3
+console.log(lengthOfLongestSubstring('pwwkew')); //3
+console.log(lengthOfLongestSubstring('bbb')); //1
