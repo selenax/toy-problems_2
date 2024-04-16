c1a4fd7fdefc2661131d89d681b45571e854222a
Selena X <selenaxx20@gmail.com>
2024-04-16 16:47:55 -0500

    refactored

--- maxProfit.js
+++ maxProfit.js
@@ -20,22 +20,49 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 
 //input: array
 //output: num
-//buy = min
-//sell = max
-//buy index < sell index
-//find max value and min value in order of array
+//fnd the smallest value and biggest value based on order of array
 //return difference
+
+//brute force O(N^2)
+// const maxProfit = (prices) => {
+//   let profit = 0;
+//   let result = 0;
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       //sell - buy
+//       result = prices[j] - prices[i];
+//       profit = Math.max(profit, result);
+//     }
+//   }
+//   return profit;
+// };
+
 const maxProfit = (prices) => {
+  let buy = 0;
+  let sell = 1;
+
   let profit = 0;
-  let result = 0;
-  for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      result = prices[j] - prices[i];
-      profit = Math.max(profit, result);
+  // buy should never reach last index of prices
+  // if buy < sell , check for profit & max
+  // else sell < buy, assign buy = sell  since that's the new lower price
+  // move sell to next index to check for the new minimum or log profit
+  // after sell reaches end of array, it will return undefined
+  while (buy < prices.length) {
+    // console.log(prices[sell], sell);
+    if (prices[buy] < prices[sell]) {
+      // console.log(sell, '2');
+      profit = Math.max(profit, prices[sell] - prices[buy]);
+    } else {
+      // console.log(sell, 'final', buy);
+      buy = sell;
+      console.log(buy, sell);
     }
+    sell++;
   }
   return profit;
 };
 
-console.log(maxProfit([7, 6, 4, 3, 1])); // 0
-console.log(maxProfit([7, 1, 5, 3, 6, 4])); //6
+// console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+// console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
+// console.log(maxProfit([1])); // 0
+console.log(maxProfit([7, 3, 6])); //
