276759ded0b3242df798f2c507df1b5a05bc2db4
Selena X <selenaxx20@gmail.com>
2024-04-22 22:40:53 -0500

    refactored: sliding window & kadane

--- maxSubArray.js
+++ maxSubArray.js
@@ -43,6 +43,8 @@
 //   for (let i = 1; i < nums.length; i++) {
 //     sum += nums[i];
 //     if (sum <= nums[i]) {
+//       console.log(nums[i], 'sum');
+
 //       sum = nums[i];
 //       max = Math.max(max, sum);
 //     }
@@ -51,21 +53,55 @@
 //   return max;
 // };
 
-// refactor from above solution
+// sliding window
 const maxSubArray = (nums) => {
-  let max = nums[0];
+  let left = 0;
+  let right = 1;
   let sum = nums[0];
+  let max = nums[0];
 
-  for (let i = 1; i < nums.length; i++) {
-    //compare sum and curr value, assign higher value to become the new sum
-    sum = Math.max(nums[i] + sum, nums[i]);
-    max = Math.max(max, sum);
+  while (right < nums.length) {
+    sum = nums[right] + sum;
+    max = Math.max(sum, max);
+    if (nums[right] >= sum) {
+      left = right;
+      sum = nums[left];
+      max = Math.max(sum, max);
+    }
+    right++;
   }
-  return max;
+  return max || 0;
 };
+// //Kadane's
+// const maxSubArray = (nums) => {
+//   let local_max = nums[0];
+//   let global_max = nums[0];
 
+//   for (let i = 1; i < nums.length; i++) {
+//     local_max = Math.max(nums[i], local_max + nums[i]);
+//     if (local_max > global_max) {
+//       global_max = local_max;
+//     }
+//   }
+//   return global_max || 0;
+// };
+
+// refactor from above solution
+// const maxSubArray = (nums) => {
+//   let max = nums[0];
+//   let sum = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     sum = Math.max(nums[i] + sum, nums[i]);
+//     max = Math.max(max, sum);
+//   }
+//   return max || 0;
+// };
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); //6
 console.log(maxSubArray([1])); //1
-console.log(maxSubArray([5, 4, -1, 7, 8])); //23
 console.log(maxSubArray([-1, 0, -2])); //0
+console.log(maxSubArray([5, 4, -1, 7, 8])); //23
+console.log(maxSubArray([])); //0
 console.log(maxSubArray([1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4])); //6
+console.log(maxSubArray([-2, -3, -1])); //-1
+console.log(maxSubArray([4, -1, 2, -7, 3, 4])); //7
