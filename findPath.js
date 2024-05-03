5c72308be415cf205fe57294b8d5bf1aef7c56fd
Selena X <selenaxx20@gmail.com>
2024-05-02 22:10:33 -0500

    refactored, added edge cases

--- findPath.js
+++ findPath.js
@@ -13,21 +13,51 @@ var obj = {
     k: null,
   },
 };
+
+// const findPath = (object, path) => {
+//   let cache = object;
+//   let arr = path.split('.');
+//   for (let i = 0; i < arr.length; i++) {
+//     // null is a JS object, returns TypeError from key lookup
+//     if (cache === null && arr[i]) return undefined;
+
+//     // if key not found, path doesn't exist
+//     if (!cache.hasOwnProperty(arr[i])) return undefined;
+
+//     // reassign current obj location for next iteration lookup
+//     cache = cache[arr[i]];
+//   }
+//   return cache;
+// };
+
+//refactored
+
 const findPath = (object, path) => {
   let cache = object;
   let arr = path.split('.');
-  for (let i = 0; i < arr.length; i++) {
-    if (cache[arr[i]] === undefined) return undefined;
-    cache = cache[arr[i]];
+
+  for (var i = 0; i < arr.length; i++) {
+    try {
+      if (!current.hasOwnProperty(arr[i])) return undefined;
+      current = current[arr[i]];
+    } catch {
+      return undefined;
+    }
   }
-  return cache;
+  return current;
 };
-console.log(findPath(obj, 'a.b.c')); // 12
-console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
-console.log(findPath(obj, 'a.b.d')); // undefined
-console.log(findPath(obj, 'a.c')); // undefined
-console.log(findPath(obj, 'a.b.c.d')); // undefined
-console.log(findPath(obj, 'a.b.c.d.e')); // undefined
-console.log(findPath(obj, 'a.b.j')); //false
-console.log(findPath(obj, 'a.b.j.k')); //undefined
-console.log(findPath(obj, 'a.k')); //null
+
+// console.log(findPath(obj, 'a.b.c')); // 12
+// console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+// console.log(findPath(obj, 'a.b.d')); // undefined
+// console.log(findPath(obj, 'a.c')); // undefined
+// console.log(findPath(obj, 'a.b.c.d')); // undefined
+// console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+// console.log(findPath(obj, 'a.b.j')); //false
+// console.log(findPath(obj, 'a.b.j.k')); //undefined
+// console.log(findPath(obj, 'a.k')); //null
+
+// edge cases
+// console.log(findPath(obj, 'a.k.j')); // undefined  (throws error looking for j when k is null)
+// tip: cache become toString function - hasOwnProperty checks for direct props, returns false on inherited prop
+console.log(findPath(obj, 'a.toString')); // undefined
